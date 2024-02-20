46b8a985b7e36af98ee723031e1be54db41a3b2f
Alex Wells <awells111@users.noreply.github.com>
2024-02-19 22:01:27 -0500

    Add flyer

---
+++ Entities/Enemies/Flyer/flyer.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var base_max_speed := 200
+@onready var aggro_range: Area2D = $AggroRange
+
+var player_body: Node2D
+var _speed
+
+func _ready() -> void:
+	_speed = base_max_speed
+
+func _physics_process(_delta: float) -> void:
+	# Move toward player, vertical and horizontal
+	if player_body:
+		var direction := global_position.direction_to(player_body.global_position)
+		velocity = direction * _speed
+		move_and_slide()
+
+func _on_aggro_range_body_entered(body: Node2D) -> void:
+	# Could instead look for method
+	# if body.has_method("take_damage"): ...
+	if body.name != "Player": return
+	
+	player_body = body
