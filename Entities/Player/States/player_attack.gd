03c70753d76d2dd4ac66dce4e18ac9f6875c89d3
unknown <brokenskygames98@gmail.com>
2024-02-21 22:55:43 -0500

    add attack state

---
+++ Entities/Player/States/player_attack.gd
@@ -0,0 +1,19 @@
+extends State
+class_name PlayerAttack
+
+# get player
+@onready var player: Player = $"../.."
+
+func enter():
+	pass
+
+func physics_update(delta):
+	
+	# Animation
+	#...
+	
+	pass
+	
+
+func exit():
+	pass
