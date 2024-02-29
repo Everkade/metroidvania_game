3a9ed813014add26c83fc207d581325919c62674
Kyle Darian <kyleenarson@gmail.com>
2024-02-29 03:30:10 -0500

    Attack direction lock

--- Entities/Player/Attack States/player_punch.gd
+++ Entities/Player/Attack States/player_punch.gd
@@ -6,13 +6,19 @@ class_name PlayerPunch
 var is_action_loc := "parameters/conditions/is_action"
 var dir_loc = "parameters/set_action/punch_dir/transition_request"
 
+@export var attack_frames : 	int = 25
+var attack_time := float(attack_frames) / 60
+var attack_count : float = 0.0
+
 func enter():
-	pass
+	player.lock_direction = true
+	# Set player combo count
+	attack_count = attack_time
 
 func physics_update(delta):
 	# Animation
-	if player.attack_count > 0:
-		player.attack_count -= delta
+	if attack_count > 0:
+		attack_count -= delta
 	else:
 		Transitioned.emit(self, "action")
 	
@@ -21,3 +27,4 @@ func physics_update(delta):
 
 func exit():
 	player.animation_tree[is_action_loc] = false
+	player.lock_direction = false
